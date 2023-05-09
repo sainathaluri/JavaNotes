1107f03edf7e725f153bec0296aac9d573a54d7f
Sainath <alurusainath85@gmail.com>
2023-05-09 18:51:38 +0530

    Example for Steam api

---
+++ src/main/java/Steamapi/Demo.java
@@ -0,0 +1,36 @@
+package Steamapi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Demo {
+    public static void main(String[] args) {
+        List<Integer> number = Arrays.asList(1, 2, 3, 4, 5);
+
+        List<Integer> square = number.stream().map(x -> x * x).collect(Collectors.toList());
+        System.out.println(square);
+
+        List<String> names = Arrays.asList("Dhoni","kohli","rohit","kl rahul");
+
+        List<String> result = names.stream().filter(s -> s.startsWith("k")).collect(Collectors.toList());
+        System.out.println(result);
+
+        List<String> show = names.stream().sorted().collect(Collectors.toList());
+        System.out.println(show);
+
+        List<Integer> numbers = Arrays.asList(2,3,4,5,2);
+
+        Set<Integer> squareSet = numbers.stream().map(x->x*x).collect(Collectors.toSet());
+        System.out.println(squareSet);
+
+        number.stream().map(x->x*x).forEach(y->System.out.println(y));
+
+        int even = number.stream().filter(x->x%2==0).reduce(0,(ans,i)-> ans+i);
+
+        System.out.println(even);
+
+    }
+}
