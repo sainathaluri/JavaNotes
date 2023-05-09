e9c68312b0911deb7e3a507f6898490eef7840fa
Sainath <alurusainath85@gmail.com>
2023-05-10 00:29:01 +0530

    Example for Parallel Array

---
+++ src/main/java/ParallelArray/ParallelArraySorting.java
@@ -0,0 +1,20 @@
+package ParallelArray;
+
+import java.util.Arrays;
+
+public class ParallelArraySorting {
+    public static void main(String[] args) {
+        int[] arr = {5,8,1,0,6,9};
+
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+
+        Arrays.parallelSort(arr);
+        System.out.println("\nArray elements after sorting");
+
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+    }
+}
